eebb4c5dc66eaa451d3081bd36169513d79cc22b
Pablo Ceballos <pablo-s_972011@hotmail.com>
2022-10-23 13:01:56 -0500

    :boom: primer proyecto

---
+++ 03/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+function App() {
+    return (<h1>Hola Mundo</h1>);
+}
+
+ReactDom.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
