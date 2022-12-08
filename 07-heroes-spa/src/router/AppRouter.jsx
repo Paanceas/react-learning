de42ee664f5c415691bc80afee8387548e11f6c1
Pablo Ceballos <pablo-s_972011@hotmail.com>
2022-12-08 15:45:04 -0500

    :sparkles: Heroes APP finish

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,14 +1,30 @@
-import { Route, Routes } from "react-router-dom"
-import { HeroesRoutes } from "../heroes"
-import { LoginPage } from "../auth"
+import { Route, Routes } from "react-router-dom";
+import { HeroesRoutes } from "../heroes";
+import { LoginPage } from "../auth";
+import { PrivateRouter } from "../heroes/routes/PrivateRouter";
+import { PublicRoutes } from "../heroes/routes/PublicRoutes";
 
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path="login" element={ <LoginPage/> } ></Route>
-            <Route path="/*" element={ <HeroesRoutes /> } ></Route>
-        </Routes>
+      <Routes>
+        <Route
+          path="login"
+          element={
+            <PublicRoutes>
+              <LoginPage />
+            </PublicRoutes>
+          }
+        ></Route>
+        <Route
+          path="/*"
+          element={
+            <PrivateRouter>
+              <HeroesRoutes />
+            </PrivateRouter>
+          }
+        ></Route>
+      </Routes>
     </>
-  )
-}
+  );
+};
